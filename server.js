11c8f515cf0fa6ab51a0d0c6731edfc6eb4f1096
prakash <prakash@meark.org>
2019-05-11 23:07:39 +0530

    h

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ http.createServer(function(req, res) {
     '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
     '\n\n');
   res.end();
-}).listen('polar-badlands-67489.herokuapp.com');
+}).listen(55778,'polar-badlands-67489.herokuapp.com');
 console.log('Server running at http://127.0.0.1:8888');
 //app.use(express.static(path.join(__dirname, 'dist')));
 // app.get('*', (req, res) => {
