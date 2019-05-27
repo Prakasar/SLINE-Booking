fd292c89d78391ff220bf8c7144067d749634f9e
prakash <prakash@meark.org>
2019-05-27 17:39:11 +0530

    change sighn up

--- router/authentication/Controller/registration.js
+++ router/authentication/Controller/registration.js
@@ -56,10 +56,7 @@ var verification_code=rows[0][0]["verification_code"];
             res.send({
                 status: true,
                 message: 'Success',
-                response: {code: 1,
-                    message: "Successfully stored."}
-    
-              });
+                response:{'user_id':rows[0][0]["user_id"],'user_name':rows[0][0]["user_name"]};
         
     }).catch( err => {
         console.log(err);
