7265b970d68c8f9e8e33332b0b6dffc7ad0ce0fe
prakash <prakash@meark.org>
2019-06-26 22:18:03 +0530

    status

--- router/Controller/api.js
+++ router/Controller/api.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res) {
 });
 
 const hos_booking=require('../Controller/booking');
+const file=require('../Controller/file');
 router.get('/location_based_hospital_lists/:latitude/:longitude',
   hos_booking.location_based_hospitalList
 );
@@ -25,4 +26,12 @@ router.post('/app_usr_select_time_staus',
 router.get('/doc_booking_history/:user_id',
   hos_booking.booking_history
 );
+router.get('/doc_booking_waiting_status/:booking_on/:user_id',
+  hos_booking.doctor_appointment_waiting_status
+);
+
+router.post('/file',
+  file.profile
+);
+
 module.exports = router;
--- router/Controller/booking.js
+++ router/Controller/booking.js
@@ -187,3 +187,34 @@ console.log(appointment);
     }
  
 }
+
+
+module.exports.doctor_appointment_waiting_status = function (req, res) {
+    try{  
+        connection.query('call usp_doc_booking_waiting_status("'+req.params.booking_on+'",'+req.params.user_id+');',function (err,appointment){
+console.log(appointment);
+          if(!err){
+            res.send({
+                status: true,
+                message: 'appointment waiting status',
+                response:appointment[0],
+            });
+        }else{
+            res.send({
+                status: false,
+                message: 'error',
+                response:err
+            });
+        }
+        });
+       
+    }catch(err)
+    {
+        res.send({
+            status:false,
+            message: 'Error occred when appointment booking.js--->doctor_appointment_list',
+            response:err
+                })
+    }
+ 
+}
